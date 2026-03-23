1dbb6f7452fef2439177f09b8afe100b491a2946
aryasamaddar <bhattagublu@gmail.com>
2026-03-23 14:27:59 +0530

    minor fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,12 +18,13 @@ const server = createServer(app);
 setupWebSocket(server);
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE"],
+  allowedHeaders: ["Content-Type","Authorization"]
+}));
 app.use("/api/users", userRoutes);
 app.use("/auth", authRoutes);
 
